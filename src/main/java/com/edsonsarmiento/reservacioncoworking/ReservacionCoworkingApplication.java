2ed5c858446057997712ccead8778422f21d9b8a
edson1416 <edsonariel.sarmiento@gmail.com>
2026-07-10 11:52:00 -0600

    Commit inicial

---
+++ src/main/java/com/edsonsarmiento/reservacioncoworking/ReservacionCoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.edsonsarmiento.reservacioncoworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservacionCoworkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservacionCoworkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edsonsarmiento/reservacioncoworking/ReservacionCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonsarmiento.reservacioncoworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservacionCoworkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
